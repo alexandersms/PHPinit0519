2d5eccbae812f94281f428ad7cf673225278111b
Xander Sms Mobutu <smsxander@gmail.com>
2019-05-13 15:08:35 +0200

    evolution de la version

--- index.php
+++ index.php
@@ -14,6 +14,108 @@
 
                 echo "Hello word";
 
+                $boll = true;
+                $int = 1.2;
+
+                $content = "<p>Ceci est un paragraphe : $int</p>";
+                $content2 = '<p>Ceci est un paragraphe : $int</p>';
+
+                echo $content;
+                // die("Fin du script"); // pour mettre fin à un script
+                echo $content2;
+
+                // Les constantes
+                const NB_ARTICLES_NB = 6;
+                // define ("DATABASE_NAME")
+
+                var_dump($content);
+
+                $ctn = "Bonjour ";
+                $ctn1 = "tout le monde";
+
+                echo $ctn . $ctn1;
+
+                echo "<br>";
+
+                // incrémentation et decrémentation
+
+                $cpt = 5;
+
+                echo $cpt ++;
+                echo "<br>";
+                echo $cpt;
+                echo "<br>";
+                echo ++$cpt;
+                echo "<br>";
+                echo --$cpt;
+                echo "<br>";
+                echo $cpt--;
+                echo "<br>";
+                echo $cpt;
+
+                echo "<br>";
+
+                // $nbr = 1;
+                // $result = (1 === $nbr);
+                // var_dump ($result);
+
+                $nbr = 2;
+                $result = ($nbr === 1);
+                echo "resultat : " . $result . "|";
+
+
+                $temp = -18;
+
+                // if ($temp < 0 || $temp < 20) {
+                //     echo "<p>La temperature est bonne</p>";
+                // } elseif ($temp < 50) {
+                //     echo "<p>La temperature es trop basse</p>";
+                // } elseif ($temp = 0) {
+                //     echo "<p>La temperature est gélé</p>";
+                // } elseif ($temp >= 20 || $temp > 50) {
+                //     echo "<p>La temperature est chaude</p>";
+                // } else {
+                //     echo "<p>helllo</p>";
+                // }
+
+                // $clim = ($temp < 20)? "<p>La temperature est bonne</p>" : "<p>La temperature est chaude</p>";
+
+                // echo $clim;
+
+                // if ($temp <= 20) {
+                //     echo "<p>La temperature est bonne</p>";
+                // } elseif ($temp > 50) {
+                //     echo "<p>La temperature es trop basse</p>";
+                // } elseif ($temp < 0) {
+                //     echo "<p>La temperature est gélé</p>";
+                // } elseif ($temp >= 20){
+                //     echo "<p>La temperature est chaude</p>";
+                // } else {
+
+                // }
+
+                // if ($temp > 0 && $temp < 20) {
+                //     echo "<p>La temperature est bonne</p>";
+                // } elseif ($temp > 50) {
+                //     echo "<p>La temperature est trop chaude</p>";
+                // } elseif ($temp <= 0) {
+                //     echo "<p>La temperature est gélé</p>";
+                // } else{
+                //     echo "<p>La temperature est chaude</p>";
+                // }
+
+                if ($temp <= 0) {
+                    echo "<p>La temperature est gélé</p>";
+                } elseif ($temp < 20){
+                    echo "<p>La temperature est bonne</p>";
+                } elseif ($temp < 50) {
+                    echo "<p>La temperature est chaude</p>";
+                } else{
+                    echo "<p>La temperature est trop chaude</p>";
+                }
+
+                
+
             ?>
 
         </p>
